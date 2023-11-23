d00055e431fe238b839970018b6aaae3a53f82de
Jhoandris B <andybaronp@gmail.com>
2023-11-23 09:25:43 -0500

    Menu Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <nav className='fixed top-0 left-0 z-20 w-full border-b border-gray-200 bg-white py-2.5 px-6 sm:px-4'>
       <div className='container flex flex-wrap items-center justify-between max-w-6xl mx-auto'>
@@ -30,16 +32,11 @@ const Header = () => {
             Tu Shop
           </span>
         </Link>
-        <div className='mt-2 sm:mt-0 sm:flex md:order-2'>
-          {/* <!-- Login Button --> */}
-          <button
-            type='button'
-            className='rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg'
-          >
-            Salir
-          </button>
 
-          {/* <!-- Register Button --> */}
+        <div
+          className='mt-2 sm:mt-0 sm:flex md:order-2'
+          onClick={() => setShowMenu(!showMenu)}
+        >
           <button
             type='button'
             className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden'
@@ -62,8 +59,24 @@ const Header = () => {
             </svg>
           </button>
         </div>
-        <div className='items-center justify-between hidden w-full md:order-1 md:flex md:w-auto'>
-          <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium'>
+        <div className='hidden sm:mt-0 sm:flex md:order-2'>
+          {/* <!-- Login Button --> */}
+          <button
+            type='button'
+            className='rounde mr-3  border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg'
+          >
+            Salir
+          </button>
+        </div>
+        <div
+          className={`items-center justify-between ${
+            !showMenu ? 'hidden' : ''
+          } w-full md:order-1 md:flex md:w-auto`}
+        >
+          <ul
+            className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium'
+            onClick={() => setShowMenu(false)}
+          >
             <li>
               <Link
                 to='/'
@@ -96,6 +109,14 @@ const Header = () => {
               >
                 Favoritos
               </a>
+            </li>
+            <li className='md:hidden'>
+              <button
+                type='button'
+                className='rounde mr-3  border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg'
+              >
+                Salir
+              </button>
             </li>
           </ul>
         </div>
