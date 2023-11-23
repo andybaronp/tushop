55ed26591fdba116d400f1c63d668fb19f8768fd
Jhoandris B <andybaronp@gmail.com>
2023-11-23 16:58:24 -0500

    routin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
 import './App.css'
 import {
   Login,
@@ -13,25 +20,47 @@ import Categories from './pages/Categories'
 import Category from './pages/Category'
 import Favorites from './pages/Favorites'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/products/:id' element={<ProductDetails />} />
+        <Route path='/categories' element={<Categories />} />
+        <Route path='/category/:id' element={<Category />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Route>
+
+      <Route path='*' element={<NotFound />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </>,
+  ),
+)
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='/' element={<Home />} />
-          {/* <Route element={<Home />} /> */}
-          <Route path='/register' element={<Register />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/products/:id' element={<ProductDetails />} />
-          <Route path='/categories' element={<Categories />} />
-          <Route path='/category/:id' element={<Category />} />
-          <Route path='/favorites' element={<Favorites />} />
-        </Route>
+    <>
+      <RouterProvider router={router} />
+    </>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Layout />}>
+    //       <Route path='/' element={<Home />} />
+    //       {/* <Route element={<Home />} /> */}
+    //       <Route path='/products' element={<Products />} />
+    //       <Route path='/products/:id' element={<ProductDetails />} />
+    //       <Route path='/categories' element={<Categories />} />
+    //       <Route path='/category/:id' element={<Category />} />
+    //       <Route path='/favorites' element={<Favorites />} />
+    //     </Route>
 
-        <Route path='*' element={<NotFound />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    //     <Route path='*' element={<NotFound />} />
+    //     <Route path='/login' element={<Login />} />
+    //       <Route path='/register' element={<Register />} />
+    //   </Routes>
+    // </BrowserRouter>
   )
 }
 
