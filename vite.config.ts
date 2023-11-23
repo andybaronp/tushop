b24b2c937ceaaeb43af906dc8755af51202d45ef
Jhoandris B <andybaronp@gmail.com>
2023-11-23 13:34:00 -0500

    proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://dummyjson.com',
-
+  proxy: {
+    // desactiva el proxy de desarrollo
+    enabled: false,
+  },
   plugins: [react()],
 })
