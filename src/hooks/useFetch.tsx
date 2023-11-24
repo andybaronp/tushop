5340225554b07d3c3b8454a828714cef1e32c460
Jhoandris B <andybaronp@gmail.com>
2023-11-24 14:18:56 -0500

    test url

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -19,7 +19,7 @@ export const useFetch = <T,>(url: string, canFetch = false) => {
     setState({ data: null, loading: true, error: null })
     if (!canFetch) {
       axios
-        .get(`${base_url}${url}`)
+        .get(`https://dummyjson.com${url}`)
         .then((resp) => resp.data)
         .then((data) => {
           setState({
