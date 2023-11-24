c927622af06be958a8a7b37f4f351d9c65bab5a0
Jhoandris B <andybaronp@gmail.com>
2023-11-24 14:54:38 -0500

    Proxy base

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Todas las rutas
-      '^/': {
-        target: 'https://dummyjson.com',
-        changeOrigin: true,
-        // Opcional: agrega un encabezado de redireccionamiento
-        rewrite: (path) => `https://dummyjson.com${path}`,
-      },
-    },
-  },
+  base: 'https://dummyjson.com',
 })
