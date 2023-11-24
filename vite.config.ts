5b76e9b78b0c720f626b1ebc7ff3b32273defe55
Jhoandris B <andybaronp@gmail.com>
2023-11-24 15:00:21 -0500

    prx

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://dummyjson.com',
+  server: {
+    proxy: {
+      '/products': {
+        target: 'https://dummyjson.com',
+        changeOrigin: true,
+      },
+    },
+  },
 })
